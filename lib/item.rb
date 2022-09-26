1be130d42f39b12d335bc185ba79e9dfde10243f
Darby Smith <newman.darby2@gmail.com>
2022-09-26 12:17:26 -0400

    Pass close_bidding

--- lib/item.rb
+++ lib/item.rb
@@ -1,16 +1,23 @@
 class Item
-  attr_reader :name, :bids
+  attr_reader :name, :bids, :bidding_closed
 
   def initialize(name)
     @name = name
     @bids = {}
+    @bidding_closed = false
   end
 
   def add_bid(attendee, price)
-    @bids[attendee] = price
+    if @bidding_closed == false
+      @bids[attendee] = price
+    end
   end
 
   def current_high_bid
     @bids.values.max
+  end
+
+  def close_bidding
+    @bidding_closed = true
   end
 end
