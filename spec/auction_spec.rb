04b0242dce9ce0632bb130fdd8a193e4a241e1e1
Darby Smith <newman.darby2@gmail.com>
2022-09-26 12:04:22 -0400

    Add bidders test

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -54,6 +54,42 @@ RSpec.describe Auction do
       @item4.add_bid(@attendee3, 50)
 
       expect(@auction.unpopular_items).to eq([@item2, @item3, @item5])
+      
+      @item3.add_bid(@attendee2, 15)
+      
+      expect(@auction.unpopular_items).to eq([@item2, @item5])
     end
   end
-end
+
+  describe '#potential_revenue' do
+    it 'returns the sum of all total possible highest sale prices' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      @item4.add_bid(@attendee3, 50)
+      @item3.add_bid(@attendee2, 15)
+
+      expect(@auction.potential_revenue).to eq(87)
+    end
+  end
+
+  describe '#bidders' do
+    it 'returns an array of bidder names' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+      @item1.add_bid(@attendee1, 22)
+      @item1.add_bid(@attendee2, 20)
+      @item4.add_bid(@attendee3, 50)
+      @item3.add_bid(@attendee2, 15)
+
+      expect(@auction.bidders).to eq(['Megan', 'Bob', 'Mike'])
+    end
+  end
+end 
